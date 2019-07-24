7cb5bb8c0cfcb2281f0afa3c4c32e2e0559035c5
DESKTOP-O05IMGI\Tomas <tomasgomcas@gmail.com>
2019-07-24 18:22:57 +0200

    Create Login page

--- Proyect/Android-Ionic/src/app/app-routing.module.ts
+++ Proyect/Android-Ionic/src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   { 
     path: 'wall', 
     loadChildren: './wall/wall.module#WallPageModule' 
-  }
+  },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+
 ];
 
 @NgModule({
